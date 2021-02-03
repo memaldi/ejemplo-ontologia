b88e748aba8bfea915be1731b308106a7c50c631
Mikel Emaldi <m.emaldi@gmail.com>
2021-02-03 14:05:38 +0100

    Corrigiendo la ruta de la ontología

--- src/test/java/OntologiaTest.java
+++ src/test/java/OntologiaTest.java
@@ -36,7 +36,7 @@ public class OntologiaTest {
     		// First, we create an OWLOntologyManager object. The manager will load and save ontologies.
         	OWLOntologyManager m=OWLManager.createOWLOntologyManager();
         	// We use the OWL API to load the Pizza ontology.
-        	OWLOntology o=m.loadOntologyFromOntologyDocument(new File("/source/ejemplo.ttl"));
+        	OWLOntology o=m.loadOntologyFromOntologyDocument(new File("ejemplo.ttl"));
         	// Now, we instantiate HermiT by creating an instance of the Reasoner class in the package org.semanticweb.HermiT.
         	Reasoner hermit=new Reasoner(o);
         	// Finally, we output whether the ontology is consistent.
